de2b609400bf6899b26e54b9ef7c752fbc9d968e
Milan Straka <fox@ucw.cz>
2014-02-17 23:51:05 +0100

    Add initial version of english_morpho.
    Still not feature complete -- numbers and punctuation not working.

---
+++ src/morpho/english_morpho.cpp
@@ -0,0 +1,147 @@
+// This file is part of MorphoDiTa.
+//
+// Copyright 2013 by Institute of Formal and Applied Linguistics, Faculty of
+// Mathematics and Physics, Charles University in Prague, Czech Republic.
+//
+// MorphoDiTa is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as
+// published by the Free Software Foundation, either version 3 of
+// the License, or (at your option) any later version.
+//
+// MorphoDiTa is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with MorphoDiTa.  If not, see <http://www.gnu.org/licenses/>.
+
+#include <algorithm>
+#include <memory>
+
+#include "english_morpho.h"
+#include "english_lemma_addinfo.h"
+#include "morpho_dictionary.h"
+#include "tag_filter.h"
+#include "tokenizer/english_tokenizer.h"
+#include "utils/compressor.h"
+#include "utils/utf8.h"
+
+namespace ufal {
+namespace morphodita {
+
+bool english_morpho::load(FILE* f) {
+  binary_decoder data;
+  if (!compressor::load(f, data)) return false;
+
+  try {
+    // Load dictionary
+    dictionary.load(data);
+  } catch (binary_decoder_error&) {
+    return false;
+  }
+
+  return data.is_end();
+}
+
+int english_morpho::analyze(string_piece form, guesser_mode guesser, vector<tagged_lemma>& lemmas) const {
+  lemmas.clear();
+
+  if (form.len) {
+    // Start by calling analyze_special without any casing changes.
+    analyze_special(form, lemmas);
+    if (!lemmas.empty()) return NO_GUESSER;
+
+    // Generate all casing variants if needed (they are different than given form).
+    string form_lc;   // all lowercase
+    generate_lc_if_needed(form, form_lc);
+
+    // Start by analysing using the dictionary and all casing variants.
+    dictionary.analyze(form, lemmas);
+    if (!form_lc.empty()) dictionary.analyze(form_lc, lemmas);
+    if (!lemmas.empty()) return NO_GUESSER;
+
+//    // For the prefix guesser, use only form_lc.
+    if (guesser == GUESSER) {}
+//      prefix_guesser->analyze(form_lc.empty() ? form : form_lc, lemmas);
+    if (!lemmas.empty()) return GUESSER;
+  }
+
+  lemmas.emplace_back(string(form.str, form.len), unknown_tag);
+  return -1;
+}
+
+int english_morpho::generate(string_piece lemma, const char* tag_wildcard, morpho::guesser_mode /*guesser*/, vector<tagged_lemma_forms>& forms) const {
+  forms.clear();
+
+  tag_filter filter(tag_wildcard);
+
+  if (lemma.len) {
+    if (dictionary.generate(lemma, filter, forms))
+      return NO_GUESSER;
+  }
+
+  return -1;
+}
+
+int english_morpho::raw_lemma_len(string_piece lemma) const {
+  return english_lemma_addinfo::raw_lemma_len(lemma);
+}
+
+int english_morpho::lemma_id_len(string_piece lemma) const {
+  return english_lemma_addinfo::lemma_id_len(lemma);
+}
+
+tokenizer* english_morpho::new_tokenizer() const {
+  return new english_tokenizer();
+}
+
+void english_morpho::analyze_special(string_piece form, vector<tagged_lemma>& lemmas) const {
+  // Analyzer for numbers and punctuation.
+  // Number is anything matching [+-]? is_Pn* ([.,] is_Pn*)? ([Ee] [+-]? is_Pn+)? for at least one is_Pn* nonempty.
+  // Punctuation is any form beginning with either unicode punctuation or punctuation_exceptions character.
+  // Beware that numbers takes precedence, so - is punctuation, -3 is number, -. is punctuation, -.3 is number.
+  if (!form.len) return;
+
+  string_piece form_ori = form;
+  char32_t first = utf8::decode(form.str, form.len);
+
+  // Try matching a number.
+  char32_t codepoint = first;
+  bool any_digit = false;
+  if (codepoint == '+' || codepoint == '-') codepoint = utf8::decode(form.str, form.len);
+  while (utf8::is_N(codepoint)) any_digit = true, codepoint = utf8::decode(form.str, form.len);
+  if (codepoint == '.' || codepoint == ',') codepoint = utf8::decode(form.str, form.len);
+  while (utf8::is_N(codepoint)) any_digit = true, codepoint = utf8::decode(form.str, form.len);
+  if (codepoint == 'e' || codepoint == 'E') {
+    codepoint = utf8::decode(form.str, form.len);
+    if (codepoint == '+' || codepoint == '-') codepoint = utf8::decode(form.str, form.len);
+    any_digit = false;
+    while (utf8::is_N(codepoint)) any_digit = true, codepoint = utf8::decode(form.str, form.len);
+  }
+
+  if (!form.len && any_digit) {
+    // We found a number. If it ends by , or ., drop it.
+    if (form_ori.str[form_ori.len-1] == '.' || form_ori.str[form_ori.len-1] == ',') form_ori.len--;
+    lemmas.emplace_back(string(form_ori.str, form_ori.len), number_tag);
+  } else if (utf8::is_P(first))
+    lemmas.emplace_back(string(form_ori.str, form_ori.len), punctuation_tag);
+}
+
+void english_morpho::generate_lc_if_needed(string_piece form, string& form_lc) const {
+  bool upper_title = false;
+  {
+    string_piece form_tmp = form;
+    while (form_tmp.len && !upper_title)
+      upper_title = utf8::is_Lut(utf8::decode(form_tmp.str, form_tmp.len));
+  }
+
+  // Generate lower case variant if different from given form.
+  if (upper_title) {
+    form_lc.reserve(form.len);
+    utf8::lowercase(form.str, form.len, form_lc);
+  }
+}
+
+} // namespace morphodita
+} // namespace ufal
---
+++ src/morpho/english_morpho.h
@@ -0,0 +1,53 @@
+// This file is part of MorphoDiTa.
+//
+// Copyright 2013 by Institute of Formal and Applied Linguistics, Faculty of
+// Mathematics and Physics, Charles University in Prague, Czech Republic.
+//
+// MorphoDiTa is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as
+// published by the Free Software Foundation, either version 3 of
+// the License, or (at your option) any later version.
+//
+// MorphoDiTa is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with MorphoDiTa.  If not, see <http://www.gnu.org/licenses/>.
+
+#pragma once
+
+#include <memory>
+
+#include "common.h"
+#include "english_lemma_addinfo.h"
+#include "morpho_dictionary.h"
+
+namespace ufal {
+namespace morphodita {
+
+class english_morpho : public morpho {
+ public:
+  virtual int analyze(string_piece form, morpho::guesser_mode guesser, vector<tagged_lemma>& lemmas) const override;
+  virtual int generate(string_piece lemma, const char* tag_wildcard, guesser_mode guesser, vector<tagged_lemma_forms>& forms) const;
+  virtual int raw_lemma_len(string_piece lemma) const override;
+  virtual int lemma_id_len(string_piece lemma) const override;
+  virtual tokenizer* new_tokenizer() const override;
+
+  bool load(FILE* f);
+ private:
+  inline void analyze_special(string_piece form, vector<tagged_lemma>& lemmas) const;
+  inline void generate_lc_if_needed(string_piece form, string& form_lc) const;
+
+  morpho_dictionary<english_lemma_addinfo> dictionary;
+
+  string unknown_tag = "UNK";
+  string number_tag = "CD";
+  string open_quotation_tag = "``", close_quotation_tag = "''";
+  string open_parenthesis_tag = "(", close_parenthesis_tag = ")";
+  string comma_tag = ",", dot_tag = ".", punctuation_tag = ":";
+};
+
+} // namespace morphodita
+} // namespace ufal
