62ead0f6cc9979856edf824d21c2a4549beaf0d5
Milan Straka <fox@ucw.cz>
2014-07-25 13:31:20 +0200

    Add strip_combining_marks tool.
    To that end, unistrip.cpp from UniLib has been also added.

---
+++ src/strip_combining_marks.cpp
@@ -0,0 +1,41 @@
+// This file is part of MorphoDiTa.
+//
+// Copyright 2013 by Institute of Formal and Applied Linguistics, Faculty of
+// Mathematics and Physics, Charles University in Prague, Czech Republic.
+//
+// MorphoDiTa is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as
+// published by the Free Software Foundation, either version 3 of
+// the License, or (at your option) any later version.
+//
+// MorphoDiTa is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with MorphoDiTa.  If not, see <http://www.gnu.org/licenses/>.
+
+#include "unilib/utf8.h"
+#include "unilib/unistrip.h"
+#include "utils/input.h"
+#include "utils/parse_options.h"
+
+using namespace ufal::morphodita;
+
+int main(int argc, char* argv[]) {
+  show_version_if_requested(argc, argv);
+
+  string input, output;
+  while (getline(stdin, input)) {
+    if (!utf8::valid(input)) runtime_errorf("Input line is not in UTF-8 encoding: '%s'", input.c_str());
+
+    output.clear();
+    for (auto&& chr : utf8::decoder(input))
+      if (!unistrip::is_combining_mark(chr))
+        utf8::append(output, unistrip::strip_combining_marks(chr));
+    puts(output.c_str());
+  }
+
+  return 0;
+}
