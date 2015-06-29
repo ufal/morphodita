0c5c73e7092bcbee4753e66fef24d156b9435f7d
Milan Straka <fox@ucw.cz>
2015-06-29 20:56:55 +0200

    Finish RunMorphoCli.

--- bindings/csharp/examples/RunMorphoCli.cs
+++ bindings/csharp/examples/RunMorphoCli.cs
@@ -17,14 +17,46 @@
 // along with MorphoDiTa.  If not, see <http://www.gnu.org/licenses/>.
 
 using Ufal.MorphoDiTa;
+using System;
 
 class RunMorphoCli {
     public static int Main(string[] args) {
+        if (args.Length < 1) {
+            Console.Error.WriteLine("Usage: RunMorphoCli dict_file");
+            return 1;
+        }
+
+        Console.Error.Write("Loading dictionary: ");
         Morpho morpho = Morpho.load(args[0]);
-        ...
+        if (morpho == null) {
+            Console.Error.WriteLine("Cannot load dictionary from file '{0}'", args[0]);
+            return 1;
+        }
+        Console.Error.WriteLine("done");
+
         TaggedLemmas lemmas = new TaggedLemmas();
-        morpho.analyze("čau", Morpho.GUESSER, lemmas);
-        foreach (TaggedLemma lemma in lemmas)
-                Console.WriteLine("{0} - {1}", lemma.lemma, lemma.tag);
+        TaggedLemmasForms lemmasForms = new TaggedLemmasForms();
+        string line;
+        while ((line = Console.In.ReadLine()) != null) {
+            string[] tokens = line.Split('\t');
+
+            if (tokens.Length == 1) /* analyze */ {
+                int result = morpho.analyze(tokens[0], Morpho.GUESSER, lemmas);
+
+                string guesser = result == Morpho.GUESSER ? "Guesser " : "";
+                foreach (TaggedLemma lemma in lemmas)
+                    Console.WriteLine("{0}Lemma: {1} {2}", guesser, lemma.lemma, lemma.tag);
+            } else if (tokens.Length == 2) /* generate */ {
+                int result = morpho.generate(tokens[0], tokens[1], Morpho.GUESSER, lemmasForms);
+                String guesser = result == Morpho.GUESSER ? "Guesser " : "";
+                foreach (TaggedLemmaForms lemmaForms in lemmasForms) {
+                    Console.WriteLine("{0}Lemma: {1}", guesser, lemmaForms.lemma);
+                    foreach (TaggedForm form in lemmaForms.forms)
+                            Console.WriteLine("  {0} {1}", form.form, form.tag);
+                }
+            }
+        }
+
+        return 0;
     }
 }
