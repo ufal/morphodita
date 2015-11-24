c9cd2500a534e8a3d8de752c6793ee0f2c9bdbaf
Milan Straka <fox@ucw.cz>
2015-11-24 11:23:21 +0100

    Add operators == and != to string_piece.

--- src/tokenizer/string_piece.h
+++ src/tokenizer/string_piece.h
@@ -23,12 +23,19 @@ struct string_piece {
   string_piece() : str(nullptr), len(0) {}
   string_piece(const char* str) : str(str), len(strlen(str)) {}
   string_piece(const char* str, size_t len) : str(str), len(len) {}
-  string_piece(const std::string& str) : str(str.c_str()), len(str.size()) {}
+  string_piece(const string& str) : str(str.c_str()), len(str.size()) {}
 };
 
 inline ostream& operator<<(ostream& os, const string_piece& str) {
-  os.write(str.str, str.len);
-  return os;
+  return os.write(str.str, str.len);
+}
+
+inline bool operator==(const string_piece& a, const string_piece& b) {
+  return a.len == b.len && memcmp(a.str, b.str, a.len) == 0;
+}
+
+inline bool operator!=(const string_piece& a, const string_piece& b) {
+  return a.len != b.len || memcmp(a.str, b.str, a.len) != 0;
 }
 
 } // namespace morphodita
