81a5ce63dcbae45a99904e051c52a575d417ff1d
Milan Straka <milan@strakovi.com>
2016-05-20 19:32:57 +0200

    Add gru_tokenizer_factory_trainer.

---
+++ src/tokenizer/gru_tokenizer_factory_trainer.cpp
@@ -0,0 +1,22 @@
+// This file is part of MorphoDiTa <http://github.com/ufal/morphodita/>.
+//
+// Copyright 2015 Institute of Formal and Applied Linguistics, Faculty of
+// Mathematics and Physics, Charles University in Prague, Czech Republic.
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+#include "gru_tokenizer_factory_trainer.h"
+
+namespace ufal {
+namespace morphodita {
+
+bool gru_tokenizer_factory_trainer::train(unsigned /*version*/, const vector<tokenized_sentence>& /*data*/, ostream& /*os*/, string& error) {
+  error.clear();
+
+  return error.assign("Not implemented yet!"), false;
+}
+
+} // namespace morphodita
+} // namespace ufal
---
+++ src/tokenizer/gru_tokenizer_factory_trainer.h
@@ -0,0 +1,29 @@
+// This file is part of MorphoDiTa <http://github.com/ufal/morphodita/>.
+//
+// Copyright 2015 Institute of Formal and Applied Linguistics, Faculty of
+// Mathematics and Physics, Charles University in Prague, Czech Republic.
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+#pragma once
+
+#include "common.h"
+#include "tokenizer.h"
+
+namespace ufal {
+namespace morphodita {
+
+class tokenized_sentence {
+  u32string sentence;
+  vector<token_range> tokens;
+};
+
+class gru_tokenizer_factory_trainer {
+ public:
+  static bool train(unsigned version, const vector<tokenized_sentence>& data, ostream& os, string& error);
+};
+
+} // namespace morphodita
+} // namespace ufal
