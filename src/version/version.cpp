716909d362cb6bcc5749031374259f5400498a51
Milan Straka <milan@strakovi.com>
2016-09-22 14:13:20 +0200

    Release version 1.9.2.

--- src/version/version.cpp
+++ src/version/version.cpp
@@ -16,7 +16,7 @@ namespace ufal {
 namespace morphodita {
 
 version version::current() {
-  return {1, 9, 2, "devel"};
+  return {1, 9, 2, ""};
 }
 
 // Returns multi-line formated version and copyright string.
