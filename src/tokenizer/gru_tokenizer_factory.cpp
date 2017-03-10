dc04dda1ad2c986feee854d29d710f038c97309d
Milan Straka <milan@strakovi.com>
2017-03-10 19:23:19 +0100

    Add new version of GRU tokenizer allowing spaces in tokens.

--- src/tokenizer/gru_tokenizer_factory.cpp
+++ src/tokenizer/gru_tokenizer_factory.cpp
@@ -16,12 +16,13 @@ namespace ufal {
 namespace morphodita {
 
 tokenizer* gru_tokenizer_factory::new_tokenizer() const {
-  return new gru_tokenizer(url_email_tokenizer, segment, *network);
+  return new gru_tokenizer(url_email_tokenizer, segment, allow_spaces, *network);
 }
 
 bool gru_tokenizer_factory::load(istream& is) {
   char version;
   if (!is.get(version)) return false;
+  if (!(version >= 1 && version <= 2)) return false;
 
   binary_decoder data;
   if (!compressor::load(is, data)) return false;
@@ -29,6 +30,7 @@ bool gru_tokenizer_factory::load(istream& is) {
   try {
     url_email_tokenizer = data.next_1B();
     segment = data.next_2B();
+    allow_spaces = version >= 2 ? data.next_1B() : false /*false was default for version 1*/;
 
     network.reset(gru_tokenizer_network::load(data));
     if (!network) return false;
